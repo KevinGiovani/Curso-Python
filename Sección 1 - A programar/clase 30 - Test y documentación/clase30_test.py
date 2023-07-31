09d0a82c329c38514a94036dd943b16604920b17
KevinGiovani <66758081+KevinGiovani@users.noreply.github.com>
2023-07-30 22:44:11 -0700

    Clase #30: Pruebas y documentación
    Utilización de docstring y doctest

---
+++ Sección 1 - A programar/clase 30 - Test y documentación/clase30_test.py
@@ -0,0 +1,19 @@
+def multiplicar(num1, num2):
+    """ 
+    Función que multiplica dos numeros
+    Args:
+        num1 (int): _description_
+        num2 (int): _description_
+    Return:
+        Retorna la multiplicación de los argumentos ingresados
+    Examples:
+        >>> multiplicar(2, 3)
+        6
+        >>> multiplicar(2,4)
+        8
+        >>> multiplicar(2,4)
+        20
+    """
+    return num1*num2
+
+print(f"El resultado de la multiplicación es {multiplicar(6,12)} ")
