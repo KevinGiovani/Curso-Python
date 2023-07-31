cd16ad8a0e5a549ac49b3c13df4a072fae6818cb
KevinGiovani <66758081+KevinGiovani@users.noreply.github.com>
2023-07-30 18:48:12 -0700

    Actividad #1: Imprimir en pantalla

---
+++ Sección 1 - A programar/Actividades/actividad1.py
@@ -0,0 +1,19 @@
+""" 
+Ejercicio 01. Imprimir en pantalla
+Mediante la terminal del sistema o la consola de Python, 
+imprimir algunas frases como las indicadas en el enunciado que tienes en el siguiente enlace: 
+https://josecodetech.blogspot.com/2022/06/ejercicio-python-01-imprimir-en-pantalla.html
+"""
+__author__ = "Kevin G. Inzunza R."
+__version__ = "1.0.0"
+
+def main():
+    """
+    Muestra en la terminal diferentes frases tipicas de
+    inicialización en un lenguaje de programación
+    """
+    print("Hola, estoy empezando en Python")
+    print("Me llamo Kevin G.")
+    print("Hola mundo")
+    
+main()
